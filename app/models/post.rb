d21ec8b589381a819e1a616e2ac3ef8c9948713e
Chanson9892 <chan.hanson5@gmail.com>
2020-09-21 15:53:55 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    # validates :clickbait
+
+    # def clickbait
+    #     if !title.include?("Won't Believe" || "Secret" || "Top" || "Guess")
+    #         errors.add(:title, "this is not clickbait")
+    #     end
+    # end
 end
+
